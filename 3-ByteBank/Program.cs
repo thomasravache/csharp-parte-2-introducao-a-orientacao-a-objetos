e0c549c68b61142547ded4e4c7c66d5538c1cabe
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-05-03 17:19:23 -0300

    feat: project 3

--- 3-ByteBank/Program.cs
+++ 3-ByteBank/Program.cs
@@ -10,6 +10,22 @@ namespace _3_ByteBank
     {
         static void Main(string[] args)
         {
+            ContaCorrente conta = new ContaCorrente();
+            conta.titular = "Teste";
+            conta.agencia = 863;
+            conta.numero = 863452;
+
+            ContaCorrente conta2 = new ContaCorrente();
+            conta2.titular = "Teste";
+            conta2.agencia = 863;
+            conta2.numero = 863452;
+
+            int idade = 1;
+            int idade2 = 1;
+
+            // Para uma classe só há um apontamento de referência e não de valor
+            Console.WriteLine(conta == conta2); // false pois conta um e conta2 são tipos de referência ou seja, a ref. da conta é diferente da conta2
+            Console.WriteLine(idade == idade2); // true pois valor 1 é igual ao valor 1
         }
     }
 }
