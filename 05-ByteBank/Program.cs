642fde02ccfcedb06cc140d558f0b4bc210d1068
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-05-04 14:55:51 -0300

    feat: project 5

---
+++ 05-ByteBank/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            ContaCorrente conta = new ContaCorrente();
+
+           // conta.titular = new Cliente();
+            if (conta.titular == null)
+            {
+                Console.WriteLine("conta.titular é nula");
+            }
+
+            conta.saldo = 500;
+            conta.agencia = 563;
+            conta.numero = 563527;
+
+            Console.WriteLine(conta.titular.nome);
+
+            Console.ReadLine();
+        }
+    }
+}
